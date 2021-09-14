20c3de038c1500b045e7f501d173209982682dc1
Jeff Loic <jeffloic@gmail.com>
2021-09-14 12:24:41 +0000

    Added search functionalities

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,34 @@
 import Image from 'next/image'
 import { SearchIcon, UsersIcon, UserCircleIcon, MenuIcon, GlobeAltIcon } from '@heroicons/react/solid'
+import { useState } from 'react'
+import { DateRangePicker } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
 
 function Header() {
+    const [searchInput, setSearchInput] = useState("");
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const [noOfGuests, setNoGuests] = useState(1);
+
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    };
+
+    const resetInput = () => {
+        setSearchInput("");
+        setStartDate(new Date());
+        setEndDate(new Date());
+    };
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: "selection",
+    }
+
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
         {/* Left */}
@@ -15,9 +42,12 @@ function Header() {
         </div>
         {/* Middle */}
         <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-           <input className="flex-grow pl-5 bg-transparent outline-none text-sm  placeholder-gray-400" 
-            type="text" 
-            placeholder="Start your search" 
+           <input 
+             value={searchInput}
+             onChange={(e) => setSearchInput(e.target.value)}
+             className="flex-grow pl-5 bg-transparent outline-none text-sm placeholder-gray-400" 
+             type="text" 
+             placeholder="Start your search" 
            />
            <SearchIcon 
             className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2 active:scale-90 transition duration-150"
@@ -33,6 +63,35 @@ function Header() {
             </div>
             
         </div>
+
+        {searchInput && (
+                <div className="flex flex-col col-span-3 mx-auto">
+                    <DateRangePicker ranges={[selectionRange]} 
+                        minDate={new Date()}
+                        rangeColors={["#FD5861"]}
+                        onChange={handleSelect}
+                    />
+                    <div className="flex items-center border-b mb-4">
+                        <h2 className="text-2xl flex-grow font-semibold">Number of Guests</h2>
+
+                        <UsersIcon 
+                          className="h-5"
+                        />
+                        <input 
+                          value={noOfGuests}
+                          min={1}
+                          onChange={e => setNoGuests(e.target.value)}
+                          type="number"
+                          className="w-12 pl-2 text-lg outline-none text-red-400"
+                        />
+                    </div> 
+                    <div className="flex">
+                        <buttom onClick={resetInput} className="flex-grow text-gray-500 cursor-pointer">Cancel</buttom>
+                        <buttom className="flex-grow text-red-400 cursor-pointer">Search</buttom>
+                    </div>        
+                </div>
+                
+            )}
            
         </header>
     )
--- components/LargeCard.js
+++ components/LargeCard.js
@@ -15,7 +15,7 @@ function LargeCard({ img, title, description, buttonText }) {
                 <h3 className="text-4xl mb-3 w-64">{title}</h3>
                 <p>{description}</p>
 
-                <button className="text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5 active:scale-90 transition duration-150">{buttonText}</button>
+                <button className="text-sm text-white bg-black px-4 py-2 rounded-lg mt-5 active:scale-90 transition duration-150">{buttonText}</button>
             </div>
         </section>
     )
